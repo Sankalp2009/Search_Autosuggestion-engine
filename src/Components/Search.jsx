ec71c9f9296cf8c7e67bcc2a7a87b9986b72578b
sankalp patel <sankalppatel38@gmail.com>
2026-05-26 16:08:35 +0530

    add data into state

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,11 +1,55 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import React, {useState, useEffect} from 'react'
 import SearchInput from './SearchInput'
 function Search() {
+
+  // Raja ke alag alag khajane ko chupane ke liye sandook (state)
   
-  // Raja ke khajane to chupane ke liye sandook(state)
   const [query, setQuery] = useState("");
-  
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] =  useState(false);
+  const [isError, setIsError] = useState(null);
+
   console.log(query);
+  
+  // API Handling
+  useEffect(()=>{
+
+    const trimmed = encodeURIComponent(query.trim());
+
+    if(!trimmed && trimmed.length < 2){
+      setResults([]);
+      setIsLoading(false);
+      setIsError(null);
+    }
+
+    const FetchSearch = async()=>{
+      try {
+        setIsLoading(true);
+        let res = await fetch(`https://dummyjson.com/products/search?q=${trimmed}`);
+        let data = await res.json();
+        console.log(data?.products);
+
+        if(!data?.products) return false;
+
+        setResults(data?.products || []);
+
+      } catch (error) {
+        console.error(error)
+        setIsError({
+          message:error.message
+        })
+      }finally{
+        setIsLoading(false);
+      }
+    }
+
+    FetchSearch();
+  },[query]);
+
+
+
+
 
   return (
     <div>
